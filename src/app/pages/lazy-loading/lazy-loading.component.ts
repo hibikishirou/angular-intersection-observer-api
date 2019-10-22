d2c6676ac2373cc39d1e6cd2983b1c6061d53ddc
longnh18 <v.longnh18@vinid.net>
2019-10-22 11:55:16 +0700

    update image handle playing

--- src/app/pages/lazy-loading/lazy-loading.component.ts
+++ src/app/pages/lazy-loading/lazy-loading.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewChildren, ViewChild, ElementRef, QueryList, AfterViewInit } from '@angular/core';
+import { Observable, concat, defer, of, fromEvent, combineLatest } from 'rxjs';
+import { map, flatMap, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-lazy-loading',
@@ -32,12 +34,39 @@ export class LazyLoadingComponent implements OnInit, AfterViewInit {
       visible: false,
     },
   ];
+
+  AUDIOLIST = [
+    {
+      url: './assets/music/Khong Sao Ma_ Em Day Roi - Suni Ha Linh_.mp3',
+      visible: false,
+      play: false
+    }
+  ];
   @ViewChildren('imgContainer') imgContainer: QueryList<ElementRef>;
+  @ViewChild('audioContainer') audioContainer: ElementRef;
   @ViewChild('loadMore') loadMore: ElementRef;
   intersectionObserver: IntersectionObserver;
+  private pageVisible$: Observable<boolean>;
+  private playing = false;
   constructor() { }
 
   ngOnInit() {
+    this.pageVisible$ = concat(
+      defer(() => of(!document.hidden)),
+      fromEvent(document, 'visibilitychange')
+        .pipe(
+          map(e => !document.hidden),
+        )
+    );
+    this.pageVisible$.subscribe(() => {
+      if (this.playing) {
+        if (document.hidden) {
+          this.audioContainer.nativeElement.pause();
+        } else {
+          this.audioContainer.nativeElement.play();
+        }
+      }
+    });
   }
 
   ngAfterViewInit() {
@@ -45,17 +74,35 @@ export class LazyLoadingComponent implements OnInit, AfterViewInit {
       this.checkForIntersection(entries);
     });
     this.intersectionObserver.observe(this.loadMore.nativeElement);
+    this.intersectionObserver.observe(this.audioContainer.nativeElement);
   }
 
   private checkForIntersection = (entries: Array<IntersectionObserverEntry>) => {
-    console.log(entries);
     entries.forEach((entry: IntersectionObserverEntry) => {
-      if (entry.intersectionRatio > 0) {
-        const result = this.IMGLIST.find(item => !item.visible);
-        if (result) {
-          result.visible = true;
-        }
+      const tag = entry.target.tagName;
+      switch (tag) {
+        case 'DIV':
+          if (entry.intersectionRatio > 0) {
+            const result = this.IMGLIST.find(item => !item.visible);
+            if (result) {
+              result.visible = true;
+            }
+          }
+          break;
+        case 'AUDIO':
+          if (this.audioContainer) {
+            if (entry.intersectionRatio === 0) {
+              this.audioContainer.nativeElement.pause();
+              this.playing = false;
+            }
+            if (entry.intersectionRatio > 0) {
+              this.audioContainer.nativeElement.play();
+              this.playing = true;
+            }
+          }
+          break;
       }
+
     });
   }
 
